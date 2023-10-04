bc9e6aa365fd8b92e86ea4b25391c084c23b2e16
KATE_FISCHER <KATME2006@GMAIL.COM>
2023-10-04 15:58:05 -0500

    Factorial Algorithm Completed and Tests Begun. JEST Downloaded.

--- factorial.js
+++ factorial.js
@@ -1,5 +1,21 @@
-function factorial(num) {
-  return 0;
+const factorial = (num) => {
+
+  if (num === 0) {
+    return 1;
+  }
+
+  let sum = 1;
+  let multiplier = 1;
+  for (i = 1; i <= num; i++) {
+    multiplier = i;
+    sum *= multiplier;
+  }
+
+  return sum;
 }
+
+console.log(factorial(0)); //1
+console.log(factorial(3)); //6
+console.log(factorial(7)); //5040
 
 module.exports = factorial;
---
+++ factorialSpec.test.js
@@ -0,0 +1,23 @@
+const factorial = require("./factorial.js");
+
+console.log(factorial(0) === 1);
+console.log(factorial(1) === 1);
+console.log(factorial(2) === 2);
+console.log(factorial(4) === 24);
+console.log(factorial(8) === 40320);
+console.log(factorial(18) === 6402373705728000);
+// Test how high of a number your program can calculate. Can you push it further?
+
+
+test("finds factorial of 0", ()=>{
+    expect(factorial(0)).toBe(1);
+})
+test("finds factorial of 1", ()=>{
+    expect(factorial(1)).toBe(1);
+})
+test("finds factorial of 2", ()=>{
+    expect(factorial(2)).toBe(2);
+})
+test("finds factorial of 3", ()=>{
+    expect(factorial(3)).toBe(6);
+})
