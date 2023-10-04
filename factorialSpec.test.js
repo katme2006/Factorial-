8f57053d282156ab738ab965fe37b2c64e9a7557
KATE_FISCHER <KATME2006@GMAIL.COM>
2023-10-04 16:51:02 -0500

    Tests updated

--- factorialSpec.test.js
+++ factorialSpec.test.js
@@ -1,11 +1,11 @@
 const factorial = require("./factorial.js");
 
-console.log(factorial(0) === 1);
-console.log(factorial(1) === 1);
-console.log(factorial(2) === 2);
-console.log(factorial(4) === 24);
-console.log(factorial(8) === 40320);
-console.log(factorial(18) === 6402373705728000);
+// console.log(factorial(0) === 1);
+// console.log(factorial(1) === 1);
+// console.log(factorial(2) === 2);
+// console.log(factorial(4) === 24);
+// console.log(factorial(8) === 40320);
+// console.log(factorial(18) === 6402373705728000);
 // Test how high of a number your program can calculate. Can you push it further?
 
 describe("Testing Factorial with various positive integers", () => {
@@ -19,9 +19,12 @@ describe("Testing Factorial with various positive integers", () => {
     expect(factorial(2)).toBe(2);
   });
   test("finds factorial of 3", () => {
-    expect(factorial(3)).toBe(6);
+    expect(factorial(4)).toBe(24);
   });
-  test("finds factorial of 7", () => {
-    expect(factorial(7)).toBe(5040);
+  test("finds factorial of 8", () => {
+    expect(factorial(8)).toBe(40320);
+  });
+  test("finds factorial of 18", () => {
+    expect(factorial(18)).toBe(6402373705728000);
   });
 });
